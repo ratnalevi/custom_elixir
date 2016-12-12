a6a77b79ab800adbb5d19fb37cab0eb7d9159531
Allam Levi Ratnakar <allam.levi@quikr.com>
2016-12-12 13:10:56 +0530

    First commit

---
+++ src/customElixir.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: allamlevi
+ * Date: 12/12/16
+ * Time: 12:58
+ */
+
+namespace customElixir;
+
+if(! function_exists('teja_elixir') ){
+
+    /**
+     * Get the path to a teja versioned Elixir file.
+     *
+     * @param  string  $file
+     * @param  string  $host
+     * @return string
+     *
+     * @throws \InvalidArgumentException
+     */
+    function teja_elixir( $file, $host = '' ){
+
+        if( env('APP_ENV') == 'local' || env('APP_ENV') == 'staging'  ){
+            $in_host = "";
+        } else{
+            $in_host = $host;
+        }
+
+        return $in_host . elixir( $file );
+    }
+}
